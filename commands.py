6b1b2e601f1ebfcf7c4e9c475a0fded5823f1789
Dmitrii Cherkasow <118098908+DmitriiCherkasow@users.noreply.github.com>
2023-01-11 13:08:57 +0300

    Add files via upload

---
+++ commands.py
@@ -0,0 +1,18 @@
+#create User
+#>>> from django.contrib.auth.models import User
+#>>> user = User.objects.create_user('name', 'email', 'userpassword')
+#>>> user.last_name = 'name'
+#>>> user.save()
+
+#>>> from news1.models import Category
+#>>> sport = Category.create(name = 'Спорт')
+#>>> sport.save()
+#>>> game = Category.create(name = 'Игры')
+#>>> game.save()
+#>>> music = Category.create(name = 'Музыка')
+#>>> music.save()
+#>>> movies = Category.create(name = 'Фильмы')
+#>>> movies.save()
+
+
+
